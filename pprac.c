27b44a52f775b96816ae3aef55e0400be5730f18
SaadAliBaig84 <115585560+SaadAliBaig84@users.noreply.github.com>
2022-11-10 08:49:27 -0800

    Add files via upload

---
+++ pprac.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void reversedarray(int *array, int *arr, int size);
+
+int main()
+{
+	
+
+    int arr2[4]={0};
+	
+	printf("Enter the number of elements: ");
+	int n;
+	scanf("%d", &n);
+	int array[n];
+	for(int i=0; i<n; i++)
+	{
+		printf("Enter element %d: ", i+1);
+		scanf("%d", &array[i]);
+	}
+	reversedarray(array, arr2, n);
+	for(int i=0; i<n; i++)
+	{
+		printf("%d", arr2[i]);
+	}
+	
+}
+
+
+void reversedarray(int *array, int *arr2, int size)
+{
+	int i=0;
+	array=&array[size-1];
+	while(i<size)
+	{
+		*arr2=*array;
+		array--;
+		arr2++;
+		i++;
+	}
+	
+}
